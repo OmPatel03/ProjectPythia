f05c5576559e61ce34cd17c735fb9e3d34c36a15
Om Patel <om@Oms-MacBook-Pro.local>
2023-10-08 15:52:55 -0400

    slashes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/PROJECTMAGGIE/',
+  base: "/ProjectPythia/",
   plugins: [react()],
-})
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+      // include: [/linked-dep/, /node_modules/],
+    },
+  },
+});
